97aa1d63e5cf195dc43618c48a44ab6f6d0d4a8c
madhu2810 <madhurids2892@gmail.com>
2019-07-26 15:43:15 +0530

    First Commit

---
+++ src/test/java/Scripts/DemoA.java
@@ -0,0 +1,16 @@
+package Scripts;
+
+
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoA {
+	@Test
+	public static void run() {
+		Reporter.log("run()",true);
+	}
+
+	
+
+}
